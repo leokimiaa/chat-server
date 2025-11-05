b7deb2f5a3b482a26eac25340fb32d7157138938
Albert Leonardi <albertleoonardi@gmail.com>
2025-11-05 14:51:49 +0700

    leo's update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://private-chat-amber.vercel.app",
     methods: ["GET", "POST"]
   }
 });
